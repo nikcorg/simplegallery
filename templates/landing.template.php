a269632b5dffe47f021a2efd0adf8091594afadf
Niklas Lindgren <nikc@iki.fi>
2009-04-23 09:01:33 +0000

    Minor text changes
    git-svn-id: https://svn.kapsi.fi/nikc/simplegallery/trunk@192 84d4dbe6-fdf0-4730-95b8-726c545e4803

--- templates/landing.template.php
+++ templates/landing.template.php
@@ -4,7 +4,7 @@
 	<h1><?php echo _e(is_array($siteTitle) ? current($siteTitle) : $siteTitle) ?></h1>
 	<p><?php echo _e($siteDescription) ?></p>
 	
-	<a href="<?php echo $useNiceUrls ? './index/' : './?index=' ?>"><img src="assets/img/vettis.jpg" width="499" height="650" alt="<?php echo $siteOwner ?>"></a>
+	<p><a href="<?php echo $useNiceUrls ? './index/' : './?index=' ?>">Enter</a></p>
 </div>
 
 <?php require_once 'tail.template.php'; ?>
